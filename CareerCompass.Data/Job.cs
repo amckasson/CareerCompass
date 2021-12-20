c28a151929b1b0f227fbc3a17450458acbaa3ae4
amckasson90@gmail.com <amckasson90@gmail.com>
2021-12-20 11:47:15 -0500

    Job class added to Data; ConnectionString made

---
+++ CareerCompass.Data/Job.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCompass.Data
+{
+    public class Job
+    {
+        [Key]
+        public int JobId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public string CompanyName { get; set; }
+        
+        [Required]
+        public string JobTitle { get; set; }
+
+        public string CompanyAddress { get; set; }
+
+        public string JobNotes { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+
+    }
+}
