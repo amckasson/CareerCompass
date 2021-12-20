4452a686e058753141b28378091ccd184ac454b3
amckasson90@gmail.com <amckasson90@gmail.com>
2021-12-20 11:11:49 -0500

    First Commit Scaffold Solution

---
+++ CareerCompass.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CareerCompass.WebMVC.Startup))]
+namespace CareerCompass.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
