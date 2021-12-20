7f5dcaafaf0555a6b0ffa8bf09c89645b5d11325
amckasson90@gmail.com <amckasson90@gmail.com>
2021-12-20 12:28:25 -0500

    CreateJob added; Job tab added

---
+++ CareerCompass.Models/JobModels/JobCreate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCompass.Models.JobModels
+{
+    public class JobCreate
+    {
+        [Required]
+        [Display(Name ="Company Name")]
+        public string CompanyName { get; set; }
+
+        [Display(Name = "Job Title")]
+        public string JobTitle { get; set; }
+
+        [Display(Name = "Company Address")]
+        public string CompanyAddress { get; set; }
+
+        [Display(Name = "Notes")]
+        public string JobNotes { get; set; }
+
+
+    }
+}
--- CareerCompass.WebMVC/Controllers/JobController.cs
+++ CareerCompass.WebMVC/Controllers/JobController.cs
@@ -16,5 +16,23 @@ namespace CareerCompass.WebMVC.Controllers
             var model = new JobListItem[0];
             return View(model);
         }
+
+        //GET Method; Making request to get the Create View
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(JobCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
+
     }
 }
