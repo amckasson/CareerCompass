b7f1dac8ffc6815d3f8e659ad37370f90b8bfc29
amckasson90@gmail.com <amckasson90@gmail.com>
2022-01-13 10:38:00 -0500

    JobEdit model added

---
+++ CareerCompass.Models/JobModels/JobEdit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCompass.Models.JobModels
+{
+    class JobEdit
+    {
+        public int JobId { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string JobTitle { get; set; }
+
+        public string CompanyAddress { get; set; }
+
+        public string JobNotes { get; set; }
+    }
+}
